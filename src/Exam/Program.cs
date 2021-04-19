d6f497b170ce4d37df13de85f5c14d44f63f9149
Negotiating <63296641+Negotiating@users.noreply.github.com>
2021-04-19 13:45:11 +0300

    Add files via upload

---
+++ src/Exam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = 0;
+            Console.Write("Введите количество элементов в массиве: "); quantity = Convert.ToInt32(Console.ReadLine());
+            Auto[] Autos = new Auto[quantity];
+            Auto.Read(Autos);
+            Auto.Sort(Autos);
+            Auto.Write_rezult(Autos);
+            Console.ReadLine();
+        }
+    }
+}
